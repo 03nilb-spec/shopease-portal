d6aaac16963520061573a308b15792360ce97278
Nilesh Bahirgaonkar <nileshbahirgaonkar1494@gmail.com>
2026-06-01 15:26:07 +0530

    Fix database.py

--- app/db/database.py
+++ app/db/database.py
@@ -4,8 +4,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sess
 from sqlalchemy.orm import DeclarativeBase
 
 load_dotenv()
-
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+asyncpg://postgres:postgres@localhost:5432/shopease")
 
 engine = create_async_engine(DATABASE_URL, echo=False)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
